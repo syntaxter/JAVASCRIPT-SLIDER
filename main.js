f8648b3adba403449481fdd3e7f4a8857f9162ba
syntaxter <syntaxter@outlook.com>
2021-01-01 16:19:41 -0500

    Cambios para ubicar en que posición del slider se encuentra la diapositiva con sentencias if, switch y for, me falta depurar los marcadores de posición ubicar de forma dinamica en que lugar se encutran los marcadores

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var botonBack = document.getElementById("clicBack");
 var imagen = document.getElementById("imagen");
 var imagen2 = document.getElementById("imagen2");
 var titulos = document.querySelector(".titulo")
-
+var indicador = document.getElementById("indicador")
 var arrayImages = ["slide1.svg","slide2.svg","slide3.svg","slide4.svg"]
 
 
@@ -16,6 +16,23 @@ var objeto ="";
 titulos.innerText= textos[0]
 imagen.src = arrayImages[0]
 
+//indicaodres
+var int = i;
+function indi() {
+    for (int=0; int < arrayImages.length; int++) {
+        var crea = document.createElement("span");
+        crea.setAttribute("id",int)
+        crea.setAttribute("data-test","false")
+        indicador.appendChild(crea)
+        console.log(crea)
+        spans = crea;
+        stgSpan = document.querySelectorAll("span")
+   
+    }
+}
+
+//funciones que se ejecutan cuando carga el documento
+indi()
 
 
 //variable acumula clic
@@ -24,6 +41,7 @@ botonNext.addEventListener("click", function() {
         objeto= this.id;
         slide()
         changeText(i)
+        slideSpan(i)
 })
     
 botonBack.addEventListener("click", function() {
@@ -31,6 +49,7 @@ botonBack.addEventListener("click", function() {
         objeto= this.id;
         slide()
         changeText(i)
+        slideSpan(i)
 
 })
 
@@ -53,16 +72,16 @@ function pasar() {
     imagen.src= arrayImages[i];
     tl.to(botonNext, {duration: 0.3, y: 0})
         .to(botonNext, {duration: 0.3, y: 20})
-    .to(botonNext, {duration: 0.3, x: 0});
+            .to(botonNext, {duration: 0.3, x: 0});
 
 //animacion imagenes
-    gsap.to(imagen, {opacity:0,x:200,display:"none",duration:1})
-    gsap.fromTo(imagen, {x:-500,display:"none",opacity:0}, {x:0,opacity:1,display:"block",ease:"sine",duration:1,delay:1})
+    gsap.to(imagen, {opacity:0,x:200,display:"none",duration:0.5})
+    gsap.fromTo(imagen, {x:-500,display:"none",opacity:0}, {x:0,opacity:1,display:"block",ease:"sine",duration:1.5})
     //gsap.to(imagen, {opacity:0,x:-200,display:"none",duration:1})
     //gsap.fromTo(imagen2, {x:500,display:"none",opacity:0}, {x:0,opacity:1,display:"block",ease:"sine",duration:1,delay:1})
 
     //animacion de titulos//
-    gsap.fromTo(titulos, {x:-500,display:"none",opacity:0}, {x:0,opacity:1,display:"block",ease:"sine",duration:1,delay:1.2})
+    gsap.fromTo(titulos, {x:-500,display:"none",opacity:0}, {x:0,opacity:1,display:"block",ease:"sine",duration:2})
 }
 
 function retroceder() {
@@ -72,7 +91,7 @@ function retroceder() {
      .to(botonBack, {duration: 0.3, x: 0})
 
     gsap.to(imagen, {opacity:0,x:-200,display:"none",duration:1})
-    gsap.fromTo(imagen2, {x:500,display:"none",opacity:0}, {x:0,opacity:1,display:"block",ease:"sine",duration:1,delay:1})
+    gsap.fromTo(imagen, {x:500,display:"none",opacity:0}, {x:0,opacity:1,display:"block",ease:"sine",duration:1,delay:1})
 
     //textos
     gsap.fromTo(titulos, {x:500,display:"none",opacity:0}, {x:0,opacity:1,display:"block",ease:"sine",duration:1,delay:1.2})
@@ -110,6 +129,76 @@ function changeText(i) {
 
 
 
+//cambia estilos de posicion
+
+var span1 = document.getElementById(0);
+var span2 = document.getElementById(1);
+var span3 = document.getElementById(2);
+var span4 = document.getElementById(3);
+
+
+span1.addEventListener("click",spansLs)
+span2.addEventListener("click",spansLs)
+span3.addEventListener("click",spansLs)
+span4.addEventListener("click",spansLs)
+
+function spansLs() {
+    var indicador = this.id;
+    switch(indicador) {
+        case "0": 
+        span1.dataset.test= "true";
+        span2.dataset.test= "false";
+        span3.dataset.test= "false";
+        span4.dataset.test= "false";
+        i=0
+        retroceder();
+        console.log(indicador);
+        break;
+
+        case "1": 
+        span1.dataset.test= "false";
+        span2.dataset.test= "true";
+        span3.dataset.test= "false";
+        span4.dataset.test= "false";
+        i=1
+        retroceder();
+        break;
+
+        case "2": 
+        span1.dataset.test= "false";
+        span2.dataset.test= "false";
+        span3.dataset.test= "true";
+        span4.dataset.test= "false";
+        console.log(indicador);
+        i=2;
+        retroceder();
+        break;
+
+        case "3":
+        span1.dataset.test= "false";
+        span2.dataset.test= "false";
+        span3.dataset.test= "false";
+        span4.dataset.test= "true";
+        console.log(indicador);
+        retroceder();
+        i=3
+        break;
+        
+    }
+
+}
+
+function slideSpan(i) {
+    var f = 0;
+    if (stgSpan[i].id !== i ) {
+       for (f=0; f < arrayImages.length; f++) {
+            stgSpan[f].dataset.test= "false";
+       } 
+        
+       stgSpan[i].dataset.test= "true";
+ 
+    }
+}
 
 
 
